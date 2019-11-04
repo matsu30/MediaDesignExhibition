3530e7cf643d858f5f3b1fc708f91255399ca628
matsu30 <18610317m@gmail.com>
2019-11-05 00:34:41 +0900

    canvasサイズを1000＊1000に変更 生成される文字の調整 ボタンの位置とテキストエリアの位置修正

--- mobile_form/move.js
+++ mobile_form/move.js
@@ -22,14 +22,50 @@ form.addEventListener('submit', function(e) {
     console.log(body);
 
 
+
+
+        // 塗り色を決める
+        context.fillStyle = "#F5D8E8";
+        // canvasの左から0、上から0の位置に幅 width、高さ heightの塗りつぶしの四角形を描く
+        context.fillRect(0, 0, 1000, 1000);
+    
+        // 以降の塗り色を決める
+        context.fillStyle = "#000000";
+    
+        context.font = "bold 600px sans-serif";
+        context.textAlign = 'center';
+        context.textBaseline = 'middle';
+        context.fillStyle = '#EDA8CD';
+        context.fillText(title, 500, 500, 1000);
+    
+        // 空のimg要素を作る
+        const img = new Image();
+    
+        img.onload = function() {
+        // img要素の読み込みが終わったらこの中が処理される
+        // 画像をHTMLに追加する
+        document.body.appendChild(img);
+        };
+    
+        // img要素 srcに画像化（base64化）したcanvasの内容を反映する
+        img.src = canvas.toDataURL('image/jpeg');
+    
+        console.log(img.src);
+        
+
+    
+
+
     //canvasの内容をbase64（文字列）にしてbase64という変数に入れておく
     const base64 = canvas.toDataURL('image/jpeg');
     
-    axios.post(url, {
-        title: title,
-        body: body, // 送信するデータPHP側で$_POSST["body"]で受け取れる
-        base64: base64 // 送信するデータPHP側で$_POSST["base64"]で受け取れる
-    })
+    const params = new URLSearchParams();
+    params.append('title', title);
+    params.append('body', body);
+    params.append('base64', base64);
+
+
+    axios.post(url, params)
         .then(function (response) {
         // データの送信に成功したときの処理をここに書く
             console.log(response);
@@ -39,7 +75,14 @@ form.addEventListener('submit', function(e) {
             console.log(error);
         });
 
+        console.log(`
+        ${url} に以下のデータを送ります
+        ${title}
+        ${body}
+        ${base64}
+        `);
 });
+
 
 
 
