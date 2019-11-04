f163bfb68bacf35a120a010ab27833687a35238e
matsu30 <18610317m@gmail.com>
2019-11-04 16:32:35 +0900

    axios.post(url

--- mobile_form/move.js
+++ mobile_form/move.js
@@ -12,6 +12,8 @@ form.addEventListener('submit', function(e) {
     // データの送信をキャンセルする
     e.preventDefault();
 
+    const url = form.action;
+
     // 入力された文字列を取得する
     const title = input.value;
     console.log(title);
@@ -23,7 +25,19 @@ form.addEventListener('submit', function(e) {
     //canvasの内容をbase64（文字列）にしてbase64という変数に入れておく
     const base64 = canvas.toDataURL('image/jpeg');
     
-    console.log(axios);
+    axios.post(url, {
+        title: title,
+        body: body, // 送信するデータPHP側で$_POSST["body"]で受け取れる
+        base64: base64 // 送信するデータPHP側で$_POSST["base64"]で受け取れる
+    })
+        .then(function (response) {
+        // データの送信に成功したときの処理をここに書く
+            console.log(response);
+        })
+        .catch(function (error) {
+        // データの送信に失敗したときの処理をここに書く
+            console.log(error);
+        });
 
 });
 
